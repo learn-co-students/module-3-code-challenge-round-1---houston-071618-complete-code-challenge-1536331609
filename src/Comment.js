33f54a9e731c39c3c1da0aa2cb282e279477a9d1
Jeannie Kim <SJ.Jeannie.Kim@gmail.com>
2018-09-07 11:19:26 -0500

    X_x;

--- src/Comment.js
+++ src/Comment.js
@@ -1,3 +1,7 @@
-class Comment {
-  
-}
+// class Comment {
+//   constructor(image){
+//     "id" = image.comments[0].id;
+//     "content": image.comments[0].content}`;
+//     "image_id": image.id,
+//   }
+// }
