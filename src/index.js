11791a3036e6d8eaa6c4c8cd82c83ec3eb51363f
ELehotzky <Lehotzky.Erica@gmail.com>
2018-09-07 11:17:46 -0500

    code challenge attempt 1

--- src/index.js
+++ src/index.js
@@ -1,14 +1,69 @@
-document.addEventListener('DOMContentLoaded', function() {
+console.log("DOM loaded");
 
-  const imageId = 1 //Enter your assigned imageId here
+	const imageId = 117 //Enter your assigned imageId here
+	const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
+	const likeURL = `https://randopic.herokuapp.com/likes/`
+	const commentsURL = `https://randopic.herokuapp.com/comments/`
 
-  const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
+document.addEventListener("click", like);
 
-  const likeURL = `https://randopic.herokuapp.com/likes/`
+fetch (`${imageURL}`)
+	.then(resp => resp.json())
+	.then(addPicToPage)
 
-  const commentsURL = `https://randopic.herokuapp.com/comments/`
+function addPicToPage(pic) {
+	let image = document.getElementById("image");
+	image.innerHTML = `
+		<div id="image_card" class="card col-md-4">
+			<img id="image" src="${pic.url}" data-id/>
+			<h4 id="${pic.name}"></h4>
+			<span>Likes:
+			<span id="${pic.likes}">0</span>
+			</span>
+		<button id="like_button">Like</button>
+	`
+	image.append()
+}
+
+function like(event) {
+	if (event.target.id === "like_button") {
+	event.preventDefault();
+	let totalLikes=parseInt(event.target)
+
+	fetch(`{likeURL}`, {
+		method: "PATCH",
+		body: JSON.stringify({ 
+			Likes: totalLikes
+		}),
+		headers: {
+			"Content-Type": "application/json"
+		}
+		.then(resp => resp.json())
+		.then(console.log)
+	})
+}}
+
+function newComment(event) {
+	let comment = document.getElementById("comment_input").value;
+	event.preventDefault();
+	fetch(`${commentsURL}`, {
+		method: "POST", 
+		body: JSON.stringify({
+			comment: comment
+		}),
+		headers: {
+			"Content-Type": "application/json"
+		}
+	})
+	.then(resp => resp.json())
+	.then(console.log)
+}
 
 
 
-})
+
+
+
+
+
 
