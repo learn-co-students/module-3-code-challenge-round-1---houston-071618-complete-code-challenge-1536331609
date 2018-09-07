e6eb8c7b105a6ae124b3980ff3d176400103b926
danrodz <daniel.a.rodriguez@dr.com>
2018-09-07 11:20:08 -0500

    Done.

--- src/Image.js
+++ src/Image.js
@@ -1,4 +1,65 @@
 class Image {
-  
+  constructor({
+    id,
+    url,
+    name,
+    like_count = 0,
+    comments = []
+  } = {}) {
+    this.id = id;
+    this.url = url;
+    this.name = name;
+    this.likeCount = like_count;
+    this.comments = this.createComments(comments);
+    Image.current.push(this);
+  }
 
+  createComments(comments) {
+    return comments.map(comment => new Comment(comment));
+  }
+
+  attachToDom() {
+    const containerForElement = document.getElementById('image_content');
+    const newImageElement = this.render();
+    containerForElement.innerHTML = newImageElement;
+  }
+
+  static likeImage() {
+    let image = Image.current[0];
+    image.likeCount++;
+    image.attachToDom();
+  }
+
+  static addComment() {
+    let image = Image.current[0];
+    let commentInput = document.getElementById('comment_input');
+    let newComment = new Comment({
+      content: commentInput.value
+    });
+    image.comments.push(newComment);
+    commentInput.value = '';
+    image.attachToDom();
+  }
+
+  render() {
+    let commentList = this.comments.map(comment => comment.render()).join('');
+    return (`
+    <div id="image_card" class="card col-md-4">
+          <img id="image" src=${this.url} data-id-${this.id}/>
+          <h4 id="name">${this.name}</h4>
+          <span>Likes:
+            <span id="likes">${this.likeCount}</span>
+          </span>
+          <button id="like_button">Like</button>
+          <form id="comment_form">
+            <input id="comment_input" type="text" name="comment" placeholder="Add Comment"/>
+            <input id="submit_button" type="submit" value="Submit"/>
+          </form>
+          <ul id="comments">
+            ${commentList}
+          </ul>
+    `);
+  }
 }
+
+Image.current = [];
--- src/index.js
+++ src/index.js
@@ -1,14 +1,61 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
 
-  const imageId = 1 //Enter your assigned imageId here
+  const imageId = 190; //Enter your assigned imageId here
+  const imageURL = `https://randopic.herokuapp.com/images/${imageId}`;
+  const likeURL = `https://randopic.herokuapp.com/likes/`;
+  const commentsURL = `https://randopic.herokuapp.com/comments/`;
 
-  const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
+  const bodyElement = document.body;
 
-  const likeURL = `https://randopic.herokuapp.com/likes/`
+  bodyElement.addEventListener('click', e => addLike(e));
 
-  const commentsURL = `https://randopic.herokuapp.com/comments/`
+  let image;
 
+  function createElements(data) {
+    image = new Image(data);
+    return image.attachToDom();
+  }
 
+  function addLike(e) {
+    e.preventDefault();
+    let targetId = e.target.id;
+    if (targetId === 'like_button') {
+      Image.likeImage();
+      return sendLikeToBackEnd();
+    } else if (targetId === 'submit_button') {
+      Image.addComment();
+      return sendLikeToBackEnd();
+    }
+  }
 
-})
+  function sendLikeToBackEnd() {
+    fetch(likeURL, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        image_id: imageId
+      })
+    }).then(resp => resp.json()).then(console.log)
+  }
 
+  function sendCommentToBackEnd() {
+    let content = image.comments[image.comments.length - 1];
+    fetch(commentsURL, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        image_id: imageId,
+        content: content
+      })
+    }).then(resp => resp.json()).then(console.log)
+  }
+
+  fetch(imageURL).then(resp => resp.json()).then(data => createElements(data));
+
+});
